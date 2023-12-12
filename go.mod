8c1f885fb5f2d25339e50cb5dc0948cd1d29b637
Resul-Necefli <necefliresul29@gmail.com>
2023-12-13 03:22:43 +0400

    YENI Kodlar

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Resul-Necefli/animalPackage
+
+go 1.21.0
---
+++ retro.go
@@ -0,0 +1,8 @@
+package retro
+
+import "strings"
+
+func retros(s string) string {
+	return "Orxan Necefli" + strings.ToUpper(s)
+
+}
